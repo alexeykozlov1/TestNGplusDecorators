f5c790fcb60c94c6c67255c0819f2c860955e3be
alexeykozlov <alexeymspm@gmail.com>
2021-03-21 17:41:24 -0400

    browser chrome 10 browsers

--- src/test/java/com/k12/ForSeleniumGrid.java
+++ src/test/java/com/k12/ForSeleniumGrid.java
@@ -19,15 +19,28 @@ public class ForSeleniumGrid {
                 AUTOMATED_BROWSER_FACTORY.getAutomatedBrowser(browser);
 
         try {
-            automatedBrowser.init();
-            automatedBrowser.goTo("https://ipublicmarketing.s3.amazonaws.com/form.html");
-
             automatedBrowser.clickElementWithId("button_element");
             assertEquals("Button Clicked", automatedBrowser.getTextFromElementWithId("message"));
             automatedBrowser.populateElementWithId("text_element", "test text");
             assertEquals("Text Input Changed", automatedBrowser.getTextFromElementWithId("message"));
 
+            automatedBrowser.populateElementWithId("textarea_element", "test text");
+            assertEquals("Text Area Changed", automatedBrowser.getTextFromElementWithId("message"));
 
+            automatedBrowser.selectOptionByTextFromSelectWithId("Option 2.1", "select_element");
+            assertEquals("Select Changed", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.clickElementWithId("radio3_element");
+            assertEquals("Radio Button Changed", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.clickElementWithId("checkbox2_element");
+            assertEquals("Checkbox Changed", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.clickElementWithId("image_element");
+            assertEquals("Image Clicked", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.clickElementWithId("div_element");
+            assertEquals("Div Clicked", automatedBrowser.getTextFromElementWithId("message"));
         } finally {
             automatedBrowser.destroy();
         }
